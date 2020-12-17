ac4b296243ef32001ce1e6ec26b31eef07f48c85
filecash <filecoincash@gmail.com>
2020-12-17 23:55:58 +0800

    merge lock code
    use passwd Encrypt pivateKey

--- chain/wallet/wallet.go
+++ chain/wallet/wallet.go
@@ -67,7 +67,12 @@ func (w *LocalWallet) WalletSign(ctx context.Context, addr address.Address, msg 
 		return nil, xerrors.Errorf("signing using key '%s': %w", addr.String(), types.ErrKeyInfoNotFound)
 	}
 
-	return sigs.Sign(ActSigType(ki.Type), ki.PrivateKey, msg)
+	pk, err := MakeByte(ki.PrivateKey, false)
+	if err != nil {
+		return nil, err
+	}
+
+	return sigs.Sign(ActSigType(ki.Type), pk, msg)
 }
 
 func (w *LocalWallet) findKey(addr address.Address) (*Key, error) {
@@ -90,6 +95,7 @@ func (w *LocalWallet) findKey(addr address.Address) (*Key, error) {
 		}
 		return nil, xerrors.Errorf("getting from keystore: %w", err)
 	}
+
 	k, err = NewKey(ki)
 	if err != nil {
 		return nil, xerrors.Errorf("decoding from keystore: %w", err)
@@ -99,7 +105,6 @@ func (w *LocalWallet) findKey(addr address.Address) (*Key, error) {
 }
 
 func (w *LocalWallet) tryFind(addr address.Address) (types.KeyInfo, error) {
-
 	ki, err := w.keystore.Get(KNamePrefix + addr.String())
 	if err == nil {
 		return ki, err
@@ -132,7 +137,13 @@ func (w *LocalWallet) tryFind(addr address.Address) (types.KeyInfo, error) {
 	return ki, nil
 }
 
-func (w *LocalWallet) WalletExport(ctx context.Context, addr address.Address) (*types.KeyInfo, error) {
+func (w *LocalWallet) WalletExport(ctx context.Context, addr address.Address, passwd string) (*types.KeyInfo, error) {
+	oldPasswd := WalletPasswd
+	WalletPasswd = passwd
+	defer func() {
+		WalletPasswd = oldPasswd
+	}()
+
 	k, err := w.findKey(addr)
 	if err != nil {
 		return nil, xerrors.Errorf("failed to find key to export: %w", err)
@@ -141,7 +152,19 @@ func (w *LocalWallet) WalletExport(ctx context.Context, addr address.Address) (*
 		return nil, xerrors.Errorf("key not found")
 	}
 
-	return &k.KeyInfo, nil
+	pk, err := MakeByte(k.PrivateKey, false)
+	if err != nil {
+		return nil, err
+	}
+	var pki types.KeyInfo
+	pki.Type = k.Type
+	pki.PrivateKey = pk
+
+	return &pki, nil
+}
+
+func (w *LocalWallet) ClearCache() {
+	w.keys = make(map[address.Address]*Key)
 }
 
 func (w *LocalWallet) WalletImport(ctx context.Context, ki *types.KeyInfo) (address.Address, error) {
@@ -152,6 +175,12 @@ func (w *LocalWallet) WalletImport(ctx context.Context, ki *types.KeyInfo) (addr
 	if err != nil {
 		return address.Undef, xerrors.Errorf("failed to make key: %w", err)
 	}
+
+	pk, err := MakeByte(k.PrivateKey, true)
+	if err != nil {
+		return address.Undef, err
+	}
+	k.PrivateKey = pk
 
 	if err := w.keystore.Put(KNamePrefix+k.Address.String(), k.KeyInfo); err != nil {
 		return address.Undef, xerrors.Errorf("saving to keystore: %w", err)
@@ -177,6 +206,7 @@ func (w *LocalWallet) WalletList(ctx context.Context) ([]address.Address, error)
 			if err != nil {
 				return nil, xerrors.Errorf("converting name to address: %w", err)
 			}
+
 			if _, ok := seen[addr]; ok {
 				continue // got duplicate with a different prefix
 			}
@@ -302,6 +332,18 @@ func (w *LocalWallet) walletDelete(ctx context.Context, addr address.Address) er
 	_ = w.keystore.Delete(KNamePrefix + tAddr)
 
 	delete(w.keys, addr)
+	return nil
+}
+
+func (w *LocalWallet) DeleteKey2(addr address.Address) error {
+	k, err := w.findKey(addr)
+	if err != nil {
+		return xerrors.Errorf("failed to delete key %s : %w", addr, err)
+	}
+
+	if err := w.keystore.Delete(KNamePrefix + k.Address.String()); err != nil {
+		return xerrors.Errorf("failed to delete key %s: %w", addr, err)
+	}
 
 	return nil
 }
@@ -316,7 +358,13 @@ func (w *LocalWallet) deleteDefault() {
 	}
 }
 
-func (w *LocalWallet) WalletDelete(ctx context.Context, addr address.Address) error {
+func (w *LocalWallet) WalletDelete(ctx context.Context, addr address.Address, passwd string) error {
+	oldPasswd := WalletPasswd
+	WalletPasswd = passwd
+	defer func() {
+		WalletPasswd = oldPasswd
+	}()
+
 	if err := w.walletDelete(ctx, addr); err != nil {
 		return xerrors.Errorf("wallet delete: %w", err)
 	}
@@ -335,6 +383,183 @@ func (w *LocalWallet) Get() api.WalletAPI {
 	}
 
 	return w
+}
+
+func (w *LocalWallet) WalletLock(ctx context.Context) error {
+	if IsSetup() {
+		if WalletPasswd != "" {
+			WalletPasswd = ""
+			return nil
+		} else {
+			return xerrors.Errorf("Wallet is locked")
+		}
+	}
+
+	return xerrors.Errorf("Passwd is not setup")
+}
+
+func (w *LocalWallet) WalletUnlock(ctx context.Context, passwd string) error {
+	if IsSetup() {
+		if WalletPasswd == "" {
+			err := CheckPasswd([]byte(passwd))
+			if err != nil {
+				return err
+			}
+			WalletPasswd = passwd
+			return nil
+		} else {
+			return xerrors.Errorf("Wallet is unlocked")
+		}
+	}
+
+	return xerrors.Errorf("Passwd is not setup")
+}
+
+func (w *LocalWallet) WalletIsLock(ctx context.Context) (bool, error) {
+	if IsSetup() {
+		if WalletPasswd == "" {
+			return true, nil
+		}
+		return false, nil
+	}
+	return false, xerrors.Errorf("Passwd is not setup")
+}
+
+func (w *LocalWallet) WalletChangePasswd(ctx context.Context, newPasswd string) (bool, error) {
+	if IsSetup() {
+		if WalletPasswd != "" {
+			addr_list, err := w.WalletList(ctx)
+
+			if err != nil {
+				return false, err
+			}
+			addr_all := make(map[address.Address]*types.KeyInfo)
+			for _, v := range addr_list {
+				addr_all[v], err = w.WalletExport(ctx, v, WalletPasswd)
+				if err != nil {
+					return false, err
+				}
+				err = w.DeleteKey2(v)
+				if err != nil {
+					return false, err
+				}
+			}
+
+			setDefault := true
+			defalutAddr, err := w.GetDefault()
+			if err != nil {
+				setDefault = false
+			}
+
+			if len(newPasswd) != 16 {
+				return false, xerrors.Errorf("passwd must 16 character")
+			}
+
+			err = ResetPasswd([]byte(newPasswd))
+			if err != nil {
+				return false, err
+			}
+
+			for k, v := range addr_all {
+				addr, err := w.WalletImport(ctx, v)
+				if err != nil {
+					return false, nil
+				} else if addr != k {
+					return false, xerrors.Errorf("import error")
+				}
+			}
+
+			if setDefault {
+				err = w.SetDefault(defalutAddr)
+				if err != nil {
+					return false, err
+				}
+			}
+
+			w.ClearCache()
+
+			return true, nil
+		}
+		return false, xerrors.Errorf("Wallet is locked")
+	}
+	return false, xerrors.Errorf("Passwd is not setup")
+}
+
+func (w *LocalWallet) WalletClearPasswd(ctx context.Context) (bool, error) {
+	if IsSetup() {
+		if WalletPasswd != "" {
+			addr_list, err := w.WalletList(ctx)
+			if err != nil {
+				return false, err
+			}
+			addr_all := make(map[address.Address]*types.KeyInfo)
+			for _, v := range addr_list {
+				addr_all[v], err = w.WalletExport(ctx, v, WalletPasswd)
+				if err != nil {
+					return false, err
+				}
+				err = w.DeleteKey2(v)
+				if err != nil {
+					return false, err
+				}
+			}
+
+			setDefault := true
+			defalutAddr, err := w.GetDefault()
+			if err != nil {
+				setDefault = false
+			}
+
+			err = ClearPasswd()
+			if err != nil {
+				return false, err
+			}
+
+			for k, v := range addr_all {
+				addr, err := w.WalletImport(ctx, v)
+				if err != nil {
+					return false, nil
+				} else if addr != k {
+					return false, xerrors.Errorf("import error")
+				}
+			}
+
+			if setDefault {
+				err = w.SetDefault(defalutAddr)
+				if err != nil {
+					return false, err
+				}
+			}
+
+			w.ClearCache()
+
+			return true, nil
+		}
+		return false, xerrors.Errorf("Wallet is locked")
+	}
+	return false, xerrors.Errorf("Passwd is not setup")
+}
+
+func (w *LocalWallet) WalletSignMessage2(ctx context.Context, k address.Address, msg *types.Message, passwd string) (*types.SignedMessage, error) {
+	mcid := msg.Cid()
+
+	oldPasswd := WalletPasswd
+	WalletPasswd = passwd
+	defer func() {
+		WalletPasswd = oldPasswd
+	}()
+
+	sig, err := w.WalletSign(ctx, k, mcid.Bytes(), api.MsgMeta{
+		Type: api.MTUnknown,
+	})
+	if err != nil {
+		return nil, xerrors.Errorf("failed to sign message: %w", err)
+	}
+
+	return &types.SignedMessage{
+		Message:   *msg,
+		Signature: *sig,
+	}, nil
 }
 
 var _ api.WalletAPI = &LocalWallet{}
